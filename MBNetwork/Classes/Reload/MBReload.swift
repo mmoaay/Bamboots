d3567f4e35c3211cf9f17d086adc3fb757723081
mmoaay <mmoaay@sina.com>
2017-01-01 21:14:08 +0800

    add reloadable

---
+++ MBNetwork/Classes/Reload/MBReload.swift
@@ -0,0 +1,15 @@
+//
+//  MBReload.swift
+//  Pods
+//
+//  Created by zhengperry on 2017/1/1.
+//
+//
+
+import Foundation
+
+public protocol MBReloadable {
+    var container: MBContainable? { get }
+    
+    func reload(error: MBErrorable)
+}
